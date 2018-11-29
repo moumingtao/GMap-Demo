0e0a6c2542f897920791e0771f12beea9ec8adbf
MingTao <MingTao@DESKTOP-L423H23>
2018-11-29 14:16:15 +0800

    添加GMap显示Google地图的简单Demo

---
+++ Sky5.GMapDemo/FrmMain.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Sky5.GMapDemo
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMap = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // gMap
+            // 
+            this.gMap.Bearing = 0F;
+            this.gMap.CanDragMap = true;
+            this.gMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMap.GrayScaleMode = false;
+            this.gMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMap.LevelsKeepInMemmory = 5;
+            this.gMap.Location = new System.Drawing.Point(13, 4);
+            this.gMap.MarkersEnabled = true;
+            this.gMap.MaxZoom = 2;
+            this.gMap.MinZoom = 2;
+            this.gMap.MouseWheelZoomEnabled = true;
+            this.gMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMap.Name = "gMap";
+            this.gMap.NegativeMode = false;
+            this.gMap.PolygonsEnabled = true;
+            this.gMap.RetryLoadTile = 0;
+            this.gMap.RoutesEnabled = true;
+            this.gMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMap.ShowTileGridLines = false;
+            this.gMap.Size = new System.Drawing.Size(506, 434);
+            this.gMap.TabIndex = 0;
+            this.gMap.Zoom = 0D;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gMap);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMap;
+    }
+}
+
---
+++ Sky5.GMapDemo/FrmMain.cs
@@ -0,0 +1,47 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sky5.GMapDemo
+{
+    public partial class FrmMain : Form
+    {
+        // 图层
+        readonly GMapOverlay top = new GMapOverlay();
+        internal readonly GMapOverlay objects = new GMapOverlay("objects");
+        internal readonly GMapOverlay routes = new GMapOverlay("routes");
+        internal readonly GMapOverlay polygons = new GMapOverlay("polygons");
+
+        // 标记
+        GMapMarker currentMarker;
+
+        // 多边形
+        GMapPolygon polygon;
+
+        // 其他
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            if (!GMapControl.IsDesignerHosted) this.Load += DesignerHost;
+        }
+
+        private void DesignerHost(object sender, EventArgs e)
+        {
+            gMap.MapProvider = GMapProviders.GoogleChinaMap;
+            gMap.Position = new PointLatLng(30.6185946, 104.0188572);
+            gMap.MinZoom = 10;
+            gMap.MaxZoom = 15;
+            gMap.Zoom = 15;
+        }
+    }
+}
